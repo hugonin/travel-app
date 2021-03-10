e590ee1a8c24ad7b3c6429cdc8ff679833051a4c
Hugonin <jeanhugues.doumbe@gmail.com>
2021-03-10 21:22:07 +0100

    updated the test for the form

--- __test__/testForm.spec.js
+++ __test__/testForm.spec.js
@@ -1,10 +1,9 @@
 import { validate } from '../src/client/js/validate';
 
 describe("Testing the form functionality", () => {
-    test("",  () => {
-        
-        expect(output).toBeDefined();
-        expect(output).toBe(true);              
+    test("should return undefined if field is empty",  () => {
+        const data = undefined;
+        expect(data).toBeUndefined;              
     });
 
 });
